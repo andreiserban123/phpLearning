072f9b5ce16c17e03ca08f6dfe69df05440c5b89
andreiserban123 <andyspeed2003@gmail.com>
2025-08-10 14:59:36 +0300

    Add initial project structure with basic routing and views

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require 'functions.php';
+//require 'router.php';
+
+
+$dsn = "mysql:host=localhost;dbname=coursedb;charset=utf8mb4";
---
+++ views/404.php
@@ -0,0 +1,12 @@
+<?php require 'views/partials/head.php'; ?>
+<?php require 'views/partials/nav.php'; ?>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1>Sorry. Page Not Found.</h1>
+
+            <p class="mt-4">
+                <a href="/" class="text-blue underline"> Go Back Home</a>
+            </p>
+        </div>
+    </main>
+<?php require 'views/partials/footer.php'; ?>
